3eaa3e8bb764ba519178a1dd7df7a9f3ed003c9f
ritishzalke <ritishzalke@gmail.com>
2024-05-06 17:27:54 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### Hangman game
+
+### License
+All of the games under the [React Puzzle Games](https://github.com/react-puzzle-games) organization are [MIT licensed](./LICENSE).
---
+++ src/AppHeader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './AppHeader.css';
+
+class AppHeader extends Component {
+  render() {
+    return (
+      <div className="App-header">
+        <h2>Hangman Game by Ritish and Salim</h2>
+      </div>
+    );
+  }
+}
+
+export default AppHeader;
